28b5f9afee8d34702c6ca93f7f3d38a889dbf507
snig-17 <snigdha.17.tiwari@icloud.com>
2024-08-01 15:48:35 +0530

    Initial Commit

---
+++ virtual dice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  virtual dice
+//
+//  Created by Snigdha Tiwari  on 01/08/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ virtual diceUITests/virtual_diceUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  virtual_diceUITestsLaunchTests.swift
+//  virtual diceUITests
+//
+//  Created by Snigdha Tiwari  on 01/08/2024.
+//
+
+import XCTest
+
+final class virtual_diceUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
